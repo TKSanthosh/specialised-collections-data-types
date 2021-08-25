6ce2ac6f6000bc180b3631cd32b7cb231e4c468e
Santhosh TK <tksanthosh494@gmail.com>
2021-08-25 20:37:09 +0530

    initial commit

--- counter.py
+++ counter.py
@@ -3,3 +3,8 @@ from collections import Counter
 a= [1,2,3,2,1,2,2,1,4,2,4,1]
 c = Counter(a)
 print(c)
+print(list(c.elements()))
+print(c.most_common())
+sub= {2:1 , 1:2}
+c.subtract(sub)
+print(c.most_common())
