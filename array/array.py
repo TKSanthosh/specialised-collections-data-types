d570ab59c80607816149aef5ece6aa79c3a3b835
Santhosh TK <tksanthosh494@gmail.com>
2021-08-26 20:33:44 +0530

    initial commit

--- array/array.py
+++ array/array.py
@@ -23,4 +23,25 @@ print(a.pop(2))  # pop function returns the value which get popped from array
 a.remove(5)  # but in remove function it wont return the value which gets removed from the array
 print(a)
 
+# array concatenation
+b = arr.array("i", [2, 4, 3, 5, 6])
+c = arr.array("i")
+c =a + b
+print(c)
+
+#array slicing
+print(a[0:5])
+
+#looping in array
+for x in a:
+    print(x)
+print("slicing")
+for x in a[1:6]:
+    print(x)
+
+print("\nwhile loop")
+f=0
+while f<len(a):
+    print(a[f])
+    f=f+1
 
