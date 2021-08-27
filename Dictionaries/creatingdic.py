39c8810c8674a373b16ed32bac840418979bc2cc
Santhosh TK <tksanthosh494@gmail.com>
2021-08-27 21:01:58 +0530

    initial commit

--- Dictionaries/creatingdic.py
+++ Dictionaries/creatingdic.py
@@ -1,3 +1,4 @@
+import pandas as pd
 my_dict= {"san": "001", "tho": "002", "sh": "003"}
 print(my_dict)
 print(type(my_dict))
@@ -5,3 +6,8 @@ print(type(my_dict))
 my_dict1= dict(san= "001", tho= "002", sh= "003")
 print(my_dict1)
 print(type(my_dict1))
+emp_details={"Employee":{"santhosh":{"id":"001","salary":"80000"},
+             "sandy":{"id":"002","salary":"70000"}}
+             }
+df = pd.DataFrame(emp_details["Employee"])
+print(df)
--- Dictionaries/deletingdicitems.py
+++ Dictionaries/deletingdicitems.py
@@ -1,4 +1,5 @@
-my_dict= {"san": "001", "tho": "002", "sh": "003"}
-
+my_dict= {"san": "001", "tho": "002", "sh": "003","tk":"004"}
+my_dict.pop("san")
 my_dict.popitem()
+del my_dict["sh"]
 print(my_dict)
